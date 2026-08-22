5b6196404dcc61d50bfa3fd2975817ff4a7bb56c
Ziyad Amara <ziyadamara@gmail.com>
2026-08-22 10:34:45 +0300

    Question 1

--- Session2/Program.cs
+++ Session2/Program.cs
@@ -1,1 +1,10 @@
 ﻿
+#region
+// 1.Write a program that allows the user to enter a number then print it. 
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("You entered: " + number);
+
+#endregion
+
