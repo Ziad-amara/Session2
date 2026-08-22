96a39ccd8fff61a9095488bf40783e88d8ef2ced
Ziyad Amara <ziyadamara@gmail.com>
2026-08-22 10:55:27 +0300

    Question 4

--- Session2/Program.cs
+++ Session2/Program.cs
@@ -33,12 +33,36 @@
 #region
 // 3. Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen 
 
-Console.Write("Enter a floating-point number:");
-float  num = float.Parse(Console.ReadLine());
+//Console.Write("Enter a floating-point number:");
+//float  num = float.Parse(Console.ReadLine());
 
-float operation = num + 0.1f;
-Console.WriteLine(operation.ToString("G9"));
-Console.WriteLine("Result of the operation: " + operation);
+//float operation = num + 0.1f;
+//Console.WriteLine(operation.ToString("G9"));
+//Console.WriteLine("Result of the operation: " + operation);
 
 // float may store decimal values as approximate binary values, so small precision differences can sometimes occur.
+#endregion
+
+#region
+// 4. Write C# program that Extract a substring from a given string. 
+
+string message = "Hello World!";  
+Console.WriteLine("the given string is " + message);
+Console.Write("Enter the chosen Word number:  ");
+
+int input = int.Parse(Console.ReadLine());
+
+if (input == 1)
+{
+    Console.WriteLine(message.Substring(0,5));
+}
+else if(input == 2)
+{
+    Console.WriteLine(message.Substring(6,5));
+        
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
 #endregion
